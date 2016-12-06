8f6eb7fef57cd158aa51d9615e239061965961c9
DARKDEAD <https://DARKDEAD@bitbucket.org/DARKDEAD/c_net.git>
2016-12-06 15:53:09 +0400

    хрень

---
+++ wndClient/ClientSocket.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace wndClient
+{
+    class ClientSocket
+    {
+        private const int port = 8888;
+        private const string server = "127.0.0.1";
+        public TcpClient client;
+        public NetworkStream stream;
+        public StringBuilder response;
+
+        public void ConnectTCP(string Login)
+        {
+
+            //пытаемся установить соединение с удаленным сервером 
+            try
+            {
+                TcpClient client = new TcpClient();
+                client.Connect(server, port);
+                                
+                StringBuilder response = new StringBuilder();
+                NetworkStream stream = client.GetStream();
+
+                // LoginForm f = new LoginForm();
+
+                // преобразуем сообщение в массив байтов
+                string message = String.Format("{0}: {1}", "Token", Login);
+                byte[] data = Encoding.Unicode.GetBytes(message);
+
+                //отправляем данные на сервер
+                SendData(data);
+                ReadData();
+
+                //Console.WriteLine(response.ToString());
+
+                // Закрываем потоки
+                //CloseStream();
+            }
+            catch (SocketException e)
+            {
+                //return (e.Message);
+            }
+            catch (Exception e)
+            {
+                //return (e.Message);
+
+            }
+        }
+
+        public bool SendData(byte[] data)
+        {
+            try
+            {
+
+                client.Client.Send(data, data.Length, 0);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        
+        //получаем данные с сервера
+        public void ReadData()
+        {           
+            do
+            {
+                byte[] data = new byte[2560];
+                int bytes = stream.Read(data, 0, data.Length);
+                response.Append(Encoding.UTF8.GetString(data, 0, bytes));
+            }
+            while (stream.DataAvailable); // пока данные есть в потоке
+        }
+
+        // Закрываем потоки
+        public void CloseStream()
+        {
+            try
+            {
+                stream.Close();
+                client.Close();
+            }
+            catch (Exception)
+            {
+
+                //throw;
+            }
+
+            
+        }
+    }
+}
---
+++ wndClient/LoginForm.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace wndClient
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtLogin = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtLogin
+            // 
+            this.txtLogin.Location = new System.Drawing.Point(38, 56);
+            this.txtLogin.Name = "txtLogin";
+            this.txtLogin.Size = new System.Drawing.Size(160, 20);
+            this.txtLogin.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(44, 122);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 28);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtLogin);
+            this.Name = "LoginForm";
+            this.Text = "Login form";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.CloseForm);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtLogin;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
